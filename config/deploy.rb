5dc963893d062eb11afda0325622338b6fe2effe
Hugh Gilmour <hugh@gilmation.com>
2011-05-20 16:36:39 +0200

    Don't delete the whole config directory only the deploy.rb which is used by Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 #require 'rvm/capistrano'
 #require 'bundler/capistrano'
 
-set :application, "ip.gilmation.com"
+set :application, "ip"
 
 # the ip or name of the domain
 set :domain, '79.125.118.188'
@@ -11,7 +11,7 @@ set :domain, '79.125.118.188'
 # by default, the name of the application (e.g. "/var/www/sites/example.com")
 set :deploy_to, "/var/www/sites/#{application}"
 
-set :repository, "git@github.com:gilmation/littlejohns_cc.git"
+set :repository, "git@github.com:gilmation/ip.git"
 
 # the branch you want to clone (default is master)
 set :branch, "master"
@@ -32,7 +32,7 @@ role :app, domain #"#{application}"
 role :web, domain #"#{application}"
 role :db,  domain, :primary => true #"#{application}", :primary => true
 
-after "deploy", "deploy:remove_non_prod_dirs"
+after "deploy", "deploy:create_sym_links", "deploy:remove_non_prod_dirs"
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
@@ -42,7 +42,12 @@ namespace :deploy do
 
   desc "Remove non production directories from the deployed application"
   task :remove_non_prod_dirs, :roles => :app do
-    run "rm -rf #{current_release}/config && rm #{current_release}/Capfile && rm #{current_release}/ip_client.rb"
+    run "rm #{current_release}/config/deploy.rb && rm #{current_release}/Capfile && rm #{current_release}/ip_client.rb && rm #{current_release}/.rvmrc"
+  end
+
+  desc "Create any symlinks that we need"
+  task :create_sym_links, :roles => :app do
+    run "ln -s #{deploy_to}/#{shared_dir}/config/basic.yml #{current_release}/config/basic.yml"
   end
 
   task :restart, :roles => :app, :except => { :no_release => true } do

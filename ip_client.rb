1dbc6b82d355590867fd822fe67ff18f15188a01
Hugh Gilmour <hugh@gilmation.com>
2023-03-15 18:13:48 +0000

    Add examples of calls to ip

--- ip_client.rb
+++ ip_client.rb
@@ -3,8 +3,9 @@ require 'open-uri'
 #
 # Get the Ip from a remote service
 #
-# open('http://localhost:4567', :http_basic_authentication=>['admin', '2jozf8xDsYpa']) do |f|
-open('http://ip.gilmation.com:80', :http_basic_authentication=>['admin', '2jozf8xDsYpa']) do |f|
+URI.open('http://127.0.0.1:9292', :http_basic_authentication=>['admin', '2jozf8xDsYpa']) do |f|
+#URI.open('http://localhost:9292', :http_basic_authentication=>['admin', '2jozf8xDsYpa']) do |f|
+#URI.open('http://ip.gilmation.com:80', :http_basic_authentication=>['admin', '2jozf8xDsYpa']) do |f|
   puts f.gets
 end
 

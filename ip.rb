da68fbdd41af63fd3c951f9e9b1dd163c4da3879
Hugh Gilmour <shugster13@gmail.com>
2023-03-25 14:24:49 +0000

    Make useage consistent everywhere

--- ip.rb
+++ ip.rb
@@ -1,32 +1,33 @@
-  # IP
-  require 'yaml'
-  require 'sinatra/base'
-  require 'figaro'
+# IP
+require "yaml"
+require "sinatra/base"
+require "figaro"
 
-  class Ip < Sinatra::Base
-
-    # Unless we have externally defined ENV variables i.e. Heroku
-    unless ENV['bearer']
-      Figaro.application = Figaro::Application.new( { 'path' => File.join( './', 'config', 'application.yml' ) } )
-      Figaro.load
-    end
-
-    before do
-      authenticate! || halt( 401, 'Access Denied' )
-    end
-
-    helpers do
-      def authenticate!
-        @bearer ||= ENV['bearer']
-        request.env['HTTP_AUTHORIZATION'] && request.env['HTTP_AUTHORIZATION'] =~ /#{@bearer}$/
-      end
-    end
-
-    # Return the IP
-    get '/' do
-      request.ip
-    end
-
-    # start the server if ruby file executed directly
-    run! if app_file == $0
+class Ip < Sinatra::Base
+  # Unless we have externally defined ENV variables i.e. Heroku
+  unless ENV["bearer"]
+    Figaro.application =
+      Figaro::Application.new(
+        { "path" => File.join("./", "config", "application.yml") }
+      )
+    Figaro.load
   end
+
+  before { authenticate! || halt(401, "Access Denied") }
+
+  helpers do
+    def authenticate!
+      @bearer ||= ENV["bearer"]
+      request.env["HTTP_AUTHORIZATION"] &&
+        request.env["HTTP_AUTHORIZATION"] =~ /^Basic.#{@bearer}$/
+    end
+  end
+
+  # Return the IP
+  get "/" do
+    request.ip
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
--- test/ip_test.rb
+++ test/ip_test.rb
@@ -1,7 +1,7 @@
-require './ip'
-require 'minitest/autorun'
-require 'minitest/unit'
-require 'rack/test'
+require "./ip"
+require "minitest/autorun"
+require "minitest/unit"
+require "rack/test"
 
 class Ip_Test < MiniTest::Unit::TestCase
   include Rack::Test::Methods
@@ -11,18 +11,18 @@ class Ip_Test < MiniTest::Unit::TestCase
   end
 
   def bearer
-    @bearer ||= ENV['bearer']
+    @bearer ||= ENV["bearer"]
     @bearer
   end
 
   def test_my_default
-    get '/'
-    assert_equal 'Access Denied', last_response.body
+    get "/"
+    assert_equal "Access Denied", last_response.body
     assert_equal 401, last_response.status
   end
 
   def test_with_http_auth
-    get '/', {}, 'HTTP_AUTHORIZATION' => bearer
+    get "/", {}, "HTTP_AUTHORIZATION" => "Basic #{bearer}"
     assert_equal "127.0.0.1", last_response.body
     assert_equal 200, last_response.status
   end
